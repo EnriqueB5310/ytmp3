16a932462765b3b9e6cb401f67b4581ffa87147c
EnriqueB5310 <124002775+EnriqueB5310@users.noreply.github.com>
2023-07-29 22:52:16 -0400

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,6 @@ convertBtn.addEventListener('click' , () => {
 })
 
 function sendURL(URL) {
-  window.location.href =`http://localhost:4000/download?URL=${URL}`;
+  window.location.href =`https://ytmp3-iqma.onrender.com/?URL=${URL}`;
 
 }
