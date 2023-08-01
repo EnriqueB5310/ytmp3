1746968df88608f7d593825f9eb4bcc2381b9626
EnriqueB5310 <124002775+EnriqueB5310@users.noreply.github.com>
2023-07-31 20:05:30 -0400

    added video downloading

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,24 @@
 const convertBtn = document.querySelector('.convert')
 const Link = document.querySelector('.URL-input')
-
+const mp4convert = document.querySelector('.mp4')
 
 convertBtn.addEventListener('click' , () => {
     sendURL(Link.value)
 })
 
+
+mp4convert.addEventListener('click', () => {
+getMp4(Link.value)
+
+})
+
 function sendURL(URL) {
   window.location.href =`https://ytmp3-iqma.onrender.com/download?URL=${URL}`;
+
+}
+
+
+function getMp4(URL) {
+  window.location.href =`https://ytmp3-iqma.onrender.com/downloadmp4?URL=${URL}`;
 
 }
