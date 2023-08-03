e98af6705bb430bede16b7d3afd7c0d5eaaee00a
EnriqueB5310 <124002775+EnriqueB5310@users.noreply.github.com>
2023-08-02 21:53:37 -0400

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,23 @@
-const convertBtn = document.querySelector('.convert')
+
+const mp3convert = document.querySelector('.mp3')
 const Link = document.querySelector('.URL-input')
 const mp4convert = document.querySelector('.mp4')
 
-convertBtn.addEventListener('click' , () => {
-    sendURL(Link.value)
+mp3convert.addEventListener('click' , () => {
+    getMP3(Link.value)
 })
-
 
 mp4convert.addEventListener('click', () => {
 getMp4(Link.value)
 
 })
 
-function sendURL(URL) {
-  window.location.href =`https://ytmp3-iqma.onrender.com/download?URL=${URL}`;
+function getMp3(URL) {
+  window.location.href =`http://localhost:4000/download?URL=${URL}`;
 
 }
 
-
 function getMp4(URL) {
-  window.location.href =`https://ytmp3-iqma.onrender.com/downloadmp4?URL=${URL}`;
+  window.location.href =`http://localhost:4000/downloadmp4?URL=${URL}`;
 
 }
